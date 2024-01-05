4210e1193ab70d11ddb39397b1981c8564a6550e
E J <johnsonelijah440@gmail.com>
2024-01-04 18:54:40 -0600

    <GA2>

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,16 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+
 public class JobTest {
     //TODO: Create your unit tests here
+
+    @Test
+    public void testSettingJobId()
+    {
+        Job job1 = new Job();
+        Job job2 = new Job();
+        job1.getId();
+
+    }
 }
